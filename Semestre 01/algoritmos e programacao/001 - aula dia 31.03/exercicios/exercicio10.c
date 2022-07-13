b083bb7ee74032568cb3ef8b1469856e9d3467a4
Vinicius Raupp <viniciusrauppcarlos@gmail.com>
2022-07-13 19:57:04 -0300

    aa

--- Semestre 01/algoritmos e programacao/001 - aula dia 31.03/exercicios/exercicio10.c
+++ Semestre 01/algoritmos e programacao/001 - aula dia 31.03/exercicios/exercicio10.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    float area,raio;
-
-    printf("Qual o raio do circulo? ");
-    scanf("%d%*f",&raio);
-
-    area=3,1415*(raio*raio);
-
-    printf("%f",area);
-    
+    printf("Hello World");
+    return 0;
 }
